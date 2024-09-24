50228896e7973f0546e3b9d0af63510e667d344a
froyangelolao <laofroy@gmail.com>
2024-09-24 09:19:34 +0800

    froy and mabby commit

---
+++ pages/Libraries_view/index.vue
@@ -0,0 +1,104 @@
+<template>
+    <NuxtLayout name="admin">
+        <main>
+            <div class="p-4">
+                <div class="flex justify-between items-center mb-4">
+                    <div>
+                        <select v-model="category" class="w-48 h-12 bg-white border border-black">
+                            <option value="city">City</option>
+                            <option value="province">Province</option>
+                            <option value="country">Country</option>
+                            <option value="nameType">Name Type</option>
+                        </select>
+                    </div>
+                    <div>
+                        <button @click="createNewEntry" class="w-24 h-12 bg-blue-500 text-white border border-black">
+                            Create
+                        </button>
+                    </div>
+                </div>
+
+                <div class="w-full border border-black">
+                    <div class="flex justify-between items-center bg-gray-800 text-white p-2 border-b border-black">
+                        <div class="font-bold">Name</div>
+                        <div class="flex space-x-2">
+                            <button @click="createEntry" class="w-16 h-8 bg-blue-500 text-white border border-black">
+                                Create
+                            </button>
+                            <button @click="viewEntry" class="w-16 h-8 bg-green-500 text-white border border-black">
+                                View
+                            </button>
+                            <button @click="updateEntry" class="w-16 h-8 bg-yellow-500 text-white border border-black">
+                                Update
+                            </button>
+                        </div>
+                    </div>
+                    <div class="p-2">
+                        <div class="flex items-center space-x-2 py-1">
+                            <input type="checkbox" v-model="cityChecked" class="form-checkbox h-5 w-5 text-blue-600">
+                            <div>City</div>
+                        </div>
+                        <div class="flex items-center space-x-2 py-1">
+                            <input type="checkbox" v-model="provinceChecked" class="form-checkbox h-5 w-5 text-blue-600">
+                            <div>Province</div>
+                        </div>
+                        <div class="flex items-center space-x-2 py-1">
+                            <input type="checkbox" v-model="countryChecked" class="form-checkbox h-5 w-5 text-blue-600">
+                            <div>Country</div>
+                        </div>
+                        <div class="flex items-center space-x-2 py-1">
+                            <input type="checkbox" v-model="nameTypeChecked" class="form-checkbox h-5 w-5 text-blue-600">
+                            <div>Name Type</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const category = ref<string>('');
+const cityChecked = ref<boolean>(false);
+const provinceChecked = ref<boolean>(false);
+const countryChecked = ref<boolean>(false);
+const nameTypeChecked = ref<boolean>(false);
+
+const getFormData = () => ({
+  category: category.value,
+  cityChecked: cityChecked.value,
+  provinceChecked: provinceChecked.value,
+  countryChecked: countryChecked.value,
+  nameTypeChecked: nameTypeChecked.value,
+});
+
+const createNewEntry = () => {
+  const data = getFormData();
+  console.log('Creating new entry:', data);
+  alert(`Created a new entry with Category: ${data.category}`);
+};
+
+const createEntry = () => {
+  const data = getFormData();
+  console.log('Creating entry:', data);
+  alert(`Created entry for Country: ${data.countryChecked}`);
+};
+
+const viewEntry = () => {
+  const data = getFormData();
+  console.log('Viewing entry:', data);
+  alert(`Viewing entry for City: ${data.cityChecked}, Province: ${data.provinceChecked}`);
+};
+
+const updateEntry = () => {
+  const data = getFormData();
+  console.log('Updating entry:', data);
+  alert(`Updated entry for Name Type: ${data.nameTypeChecked}`);
+};
+</script>
+
+<style scoped>
+/* Add any component-specific styles here */
+</style>
--- pages/customer/index.vue
+++ pages/customer/index.vue
@@ -1,129 +1,131 @@
 <template>
-    <NuxtLayout name="admin">
-    <div class="bg-white">
-      <header class="py-8 px-4 md:px-16">
-        <div class="container mx-auto flex justify-between items-center">
-          <img src="../../img/LendCash_Logo-removebg-preview.png" class="w-20" alt="logo" />
-        </div>
-      </header>
-    </div>
-  
-    <div class="max-w-screen-xl mx-auto px-4 md:px-8">
-      <div class="items-start justify-between md:flex">
-        <div class="max-w-lg">
-          <h3 class="text-gray-800 text-xl font-bold sm:text-2xl">Customers</h3>
-        </div>
-        <div class="mt-3 md:mt-0 flex flex-col items-start gap-y-4">
-          <FormButton @click="createCustomer" class="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm">
-          Add Customer
-          </FormButton>
-        </div>
+  <NuxtLayout name="admin">
+      <div class="bg-white">
+          <header class="py-8 px-4 md:px-16">
+              <div class="container mx-auto flex justify-between items-center">
+                  <img src="../../img/LendCash_Logo-removebg-preview.png" class="w-20" alt="logo" />
+              </div>
+          </header>
       </div>
 
-      <!-- Action Buttons -->
-    <div class="flex justify-between items-center mb-8">
-      <!-- Left: Action Buttons -->
-      <div class="flex space-x-4">
-        <button @click="createEmployee" class="px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600">
-          Create
-        </button>
-        <button @click="modifyEmployee" :disabled="!selectedEmployeeId" class="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 disabled:opacity-50">
-          Modify
-        </button>
-        <button @click="deleteEmployee" :disabled="!selectedEmployeeId" class="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600 disabled:opacity-50">
-          Delete
-        </button>
+      <div class="max-w-screen-xl mx-auto px-4 md:px-8">
+          <div class="items-start justify-between md:flex">
+              <div class="max-w-lg">
+                  <h3 class="text-gray-800 text-xl font-bold sm:text-2xl">Customers</h3>
+              </div>
+              <div class="mt-3 md:mt-0 flex flex-col items-start gap-y-4">
+                  <FormButton @click="createCustomer" class="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm">
+                      Add Customer
+                  </FormButton>
+              </div>
+          </div>
+
+          <!-- Action Buttons -->
+          <div class="flex justify-between items-center mb-8">
+              <!-- Left: Action Buttons -->
+              <div class="flex space-x-4">
+                  <button @click="createEmployee" class="px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600">
+                      Create
+                  </button>
+                  <button @click="modifyEmployee" :disabled="!selectedEmployeeId" class="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 disabled:opacity-50">
+                      Modify
+                  </button>
+                  <button @click="deleteEmployee" :disabled="!selectedEmployeeId" class="px-4 py-2 text-white bg-red-500 rounded-lg hover:bg-red-600 disabled:opacity-50">
+                      Delete
+                  </button>
+              </div>
+          </div>
+
+          <!-- Right: Search Bar -->
+        <div class="flex items-center space-x-2">
+          <input
+            v-model="searchQuery"
+            type="text"
+            placeholder="Search employees"
+            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none"
+          />
+          <button class="px-4 py-2 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700">
+            Search
+          </button>
+        </div>
+
+          <!-- Customer Table -->
+          <div class="mt-12 shadow-sm border rounded-lg overflow-x-auto">
+              <table class="w-full table-auto text-sm text-left">
+                  <thead class="bg-gray-50 text-gray-600 font-medium border-b">
+                      <tr>
+                          <th class="py-3 px-6">Employee ID</th>
+                          <th class="py-3 px-6">Name</th>
+                          <th class="py-3 px-6">Email</th>
+                          <th class="py-3 px-6">Telephone#</th>
+                          <th class="py-3 px-6">Birthday</th>
+                          <th class="py-3 px-6">Gender</th>
+                          <th class="py-3 px-6">Civil Status</th>
+                          <th class="py-3 px-6">Group Name</th>
+                          <th class="py-3 px-6">House Street</th>
+                          <th class="py-3 px-6">Purok Zone</th>
+                          <th class="py-3 px-6">Postal Code</th>
+                          <th class="py-3 px-6">Pesonality Status</th>
+                          <th class="py-3 px-6">Barangay</th>
+                          <th class="py-3 px-6">City</th>
+                          <th class="py-3 px-6">Country</th>
+                          <th class="py-3 px-6">Province</th>
+                          <th class="py-3 px-6">Credit Status</th>
+                          <th class="py-3 px-6">Date Created</th>
+                          <th class="py-3 px-6">Date Last Modified</th>
+                          <th class="py-3 px-6">Passbook Number</th>
+                          <th class="py-3 px-6">Loan Count</th>
+                          <th class="py-3 px-6">Mortuary Status</th>
+                          <th class="py-3 px-6">Mortuary Coverage Start</th>
+                          <th class="py-3 px-6">Mortuary Coverage End</th>
+                          <th class="py-3 px-6">Notes</th>
+                      </tr>
+                  </thead>
+                  <tbody class="text-gray-600 divide-y">
+                      <tr v-for="(item, idx) in filteredTableItems" :key="idx">
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.employeeId }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.name }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.email }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.telephoneNumber }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.birthday }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.gender }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.civilStatus }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.groupName }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.houseStreet }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.purokZone }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.postalCode }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.personalityStatus }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.Barangay }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.city }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.country }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.province }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.creditStatus }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.dateCreated }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.dateLastModified }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.passbookNumber }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.loanCount }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">
+                              <span :class="{'bg-green-500': item.mortuaryStatus, 'bg-red-500': !item.mortuaryStatus}" class="inline-block w-3 h-3 rounded-full"></span>
+                              {{ item.mortuaryStatus ? 'Enabled' : 'Disabled' }}
+                          </td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.mortuaryCoverageStart }}</td>
+                          <td class="px-6 py-4 whitespace-nowrap">{{ item.mortuaryCoverageEnd }}</td>
+                          <td class="text-right px-6 whitespace-nowrap">
+                              <button @click="editMember(item)" class="py-1.5 px-3 bg-blue-200 hover:bg-blue-300 rounded">
+                                  Edit
+                              </button>
+                          </td>
+                      </tr>
+                      <tr v-if="filteredTableItems.length === 0">
+                          <td colspan="11" class="text-center py-4 text-gray-600">No data found</td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
       </div>
-  
-      <!-- Search Bar - Centered and Spaced Above Table -->
-      <div class="flex justify-center mt-8 mb-6">
-        <input
-          v-model="searchQuery"
-          type="text"
-          placeholder="Search customer"
-          class="px-4 py-2 border border-gray-300 rounded-lg"
-        />
-      </div>
-  
-      <!-- Customer Table -->
-      <div class="mt-12 shadow-sm border rounded-lg overflow-x-auto">
-        <table class="w-full table-auto text-sm text-left">
-          <thead class="bg-gray-50 text-gray-600 font-medium border-b">
-            <tr>
-              <th class="py-3 px-6">Employee ID</th>
-              <th class="py-3 px-6">Name</th>
-              <th class="py-3 px-6">Email</th>
-              <th class="py-3 px-6">Telephone#</th>
-              <th class="py-3 px-6">Birthday</th>
-              <th class="py-3 px-6">Gender</th>
-              <th class="py-3 px-6">Civil Status</th>
-              <th class="py-3 px-6">Group Name</th>
-              <th class="py-3 px-6">House Street</th>
-              <th class="py-3 px-6">Purok Zone</th>
-              <th class="py-3 px-6">postal Code</th>
-              <th class="py-3 px-6">Pesonality Status</th>
-              <th class="py-3 px-6">Barangay</th>
-              <th class="py-3 px-6">City</th>
-              <th class="py-3 px-6">Country</th>
-              <th class="py-3 px-6">Province</th>
-              <th class="py-3 px-6">Credit Status</th>
-              <th class="py-3 px-6">Date Created</th>
-              <th class="py-3 px-6">Date Last Modified</th>
-              <th class="py-3 px-6">Passbook Number</th>
-              <th class="py-3 px-6">Loan Count</th>
-              <th class="py-3 px-6">Mortuary Status</th>
-              <th class="py-3 px-6">Mortuary Coverage Start</th>
-              <th class="py-3 px-6">Mortuary Coverage End</th>
-              <th class="py-3 px-6">Notes</th>
-              <th class="py-3 px-6">Action</th>
-            </tr>
-          </thead>
-          <tbody class="text-gray-600 divide-y">
-            <tr v-for="(item, idx) in filteredTableItems" :key="idx">
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.employeeId }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.name }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.email }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.telephoneNumber }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.birthday }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.gender }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.civilStatus }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.groupName }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.houseStreet }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.purokZone }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.postalCode }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.personalityStatus }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.Barangay }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.city }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.country }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.province }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.creditStatus }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.dateCreated }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.dateLastModified }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.passbookNumber }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.loanCount }}</td>
-              <td class="px-6 py-4 whitespace-nowrap"></td>
-              <!-- Display Mortuary Status with Color Indicator -->
-              <td class="px-6 py-4 whitespace-nowrap">
-                <span :class="{'bg-green-500': item.mortuaryStatus, 'bg-red-500': !item.mortuaryStatus}" class="inline-block w-3 h-3 rounded-full"></span>
-                {{ item.mortuaryStatus ? 'Enabled' : 'Disabled' }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.mortuaryCoverageStart }}</td>
-              <td class="px-6 py-4 whitespace-nowrap">{{ item.mortuaryCoverageEnd }}</td>
-              <td class="text-right px-6 whitespace-nowrap">
-                <button @click="editMember(item)" class="py-1.5 px-3 bg-blue-200 hover:bg-blue-300 rounded">
-                  Edit
-                </button>
-              </td>
-            </tr>
-            <tr v-if="filteredTableItems.length === 0">
-              <td colspan="11" class="text-center py-4 text-gray-600">No data found</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
   </NuxtLayout>
-  </template>
+</template>
+
   
   <script setup lang="ts">
   import { CustomersService } from '~/models/Customer';
